0f6bfa35d4fed0bfe0f866d36019293539fc1f95
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-11-01 15:32:39 +0100

    Update payview.php

---
+++ resources/lang/en/payview.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'pagename' => [
+        'login'             => 'Login with your account',
+        'register'          => 'Create an account',
+        'activate'          => 'Activate your account',
+        'dashboard'         => 'Dashboard',
+        'settings'          => 'Settings',
+    ],
+];
