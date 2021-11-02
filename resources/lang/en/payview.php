748e247113fc8fe730a732023abd912a51437bf8
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-11-02 18:13:32 +0100

    Update payview.php

--- resources/lang/en/payview.php
+++ resources/lang/en/payview.php
@@ -8,4 +8,17 @@ return [
         'dashboard'         => 'Dashboard',
         'settings'          => 'Settings',
     ],
+
+    'auth' => [
+        'login' => [
+            'title'             => 'Login with your account',
+            'description'       => 'Use the form below to log into your personal area of payView.',
+            'email'             => 'E-mail address',
+            'password'          => 'Password',
+            'submit'            => 'Sign in',
+            'account'           => 'Don\'t have an account yet?',
+            'signup'            => 'Sign up',
+            'error'             => 'Something didn\'t go quite right..',
+        ],
+    ],
 ];
