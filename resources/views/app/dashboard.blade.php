a897839c30c994cecf56567cb13589849f29e68f
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-11-13 03:25:34 +0100

    Update dashboard.blade.php

--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -2,12 +2,6 @@
 @section('pagename', __('payview.pagename.dashboard'))
 
 @section('content')
-    @if (session()->has('success'))
-        <div class="auth-card-body error success">
-            <strong>@lang('payview.auth.login.success')</strong>
-            {{ session('success') }}
-        </div>
-    @endif
 
     <div class="container">
 
